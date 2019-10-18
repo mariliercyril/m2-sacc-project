9b3eb23fad6af7dc2194ec3b9e3b54a2a366e862
LaurentBenazet <benazetlaurent@gmail.com>
2019-10-18 22:33:38 +0200

    Push queue works for sending mail

--- src/main/java/PolyUrl/MailQueue.java
+++ src/main/java/PolyUrl/MailQueue.java
@@ -1,14 +1,9 @@
 package PolyUrl;
 
-import com.google.gson.Gson;
-
-import java.io.IOException;
-
 import com.google.appengine.api.taskqueue.Queue;
 import com.google.appengine.api.taskqueue.QueueFactory;
 import com.google.appengine.api.taskqueue.TaskOptions;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,13 +12,11 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "MailQueue", value = "/mailqueue")
 public class MailQueue extends HttpServlet {
 
-    private Gson gson = new Gson();
-
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        String recipientMail = req.getParameter("recipientMail");
         Queue queue = QueueFactory.getQueue("queue-mail");
-        queue.add(TaskOptions.Builder.withUrl("/mailworker").param("key", gson.toJson("placeholder")));
+        queue.add(TaskOptions.Builder.withUrl("/mailworker").param("recipientMail", recipientMail));
     }
 }
 
--- src/main/java/PolyUrl/MailWorker.java
+++ src/main/java/PolyUrl/MailWorker.java
@@ -1,20 +1,42 @@
 package PolyUrl;
 
-import java.io.IOException;
-import java.util.logging.Logger;
-
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
 
 @WebServlet(name = "MailWorker", value = "/mailworker")
 public class MailWorker extends HttpServlet {
-    private static Logger log = Logger.getLogger(MailWorker.class.getName());
+    public void sendSimpleMail(String recipientMail) {
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+
+        try {
+            Message msg = new MimeMessage(session);
+            msg.setFrom(new InternetAddress("benazetlaurent@gmail.com", "Sender"));
+            msg.addRecipient(Message.RecipientType.TO,
+                    new InternetAddress(recipientMail, "User"));
+            msg.setSubject("Test email from gcloud push queue");
+            msg.setText("This is a test");
+            Transport.send(msg);
+        } catch (MessagingException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
-    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        System.out.println(req.getParameter("key"));
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        String recipientMail = req.getParameter("recipientMail");
+        sendSimpleMail(recipientMail);
+        System.out.println(recipientMail);
     }
 
 }
