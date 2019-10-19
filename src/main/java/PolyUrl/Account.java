67ae9faa74b665878a1bac514ea55adef16cf754
Tetevi Fiacre TOGNI <tetevi-fiacre.togni@etu.unice.fr>
2019-10-19 21:11:20 +0200

    account creation for PolyUrl

--- src/main/java/PolyUrl/Account.java
+++ src/main/java/PolyUrl/Account.java
@@ -1,9 +1,18 @@
 package PolyUrl;
 
 import com.google.gson.Gson;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import javax.servlet.ServletException;
@@ -29,7 +38,31 @@ public class Account extends HttpServlet {
     //create a new account
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //code
+
+        String name = request.getParameter("name");
+        String mail = request.getParameter("mail");
+        String admin = request.getParameter("admin");
+        String subject = "PolyUrl Account creation";
+        String message = "Welcome "+name ;
+
+
+        HttpPost post = new HttpPost("http://polyurl.appspot.com/mailqueue");
+
+        // add request parameter, form parameters
+        List<NameValuePair> urlParameters = new ArrayList<>();
+        urlParameters.add(new BasicNameValuePair("recipientMail", mail));
+        urlParameters.add(new BasicNameValuePair("subject", subject));
+        urlParameters.add(new BasicNameValuePair("message", message));
+
+
+
+        post.setEntity(new UrlEncodedFormEntity(urlParameters));
+
+        try (CloseableHttpClient httpClient = HttpClients.createDefault();
+             CloseableHttpResponse resp = httpClient.execute(post)) {
+
+            System.out.println(EntityUtils.toString(resp.getEntity()));
+        }
         response.getWriter().println("create new account");
     }
 }
