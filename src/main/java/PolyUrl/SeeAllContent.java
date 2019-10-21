a2e4673170aea0e65a43b8442f33cdde9fef485b
mariliercyril <cyril.marilier@etu.unice.fr>
2019-10-21 23:39:31 +0100

    Complete the Administration interface - Implement the classes 'SeeAllContent' and 'SeeDetails'

--- src/main/java/PolyUrl/SeeAllContent.java
+++ src/main/java/PolyUrl/SeeAllContent.java
@@ -3,23 +3,64 @@ package PolyUrl;
 import com.google.gson.Gson;
 
 import javax.servlet.annotation.WebServlet;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import java.util.stream.Collectors;
+
 
 @WebServlet(name = "SeeAllContent", value = "/seeallcontent")
 public class SeeAllContent extends HttpServlet {
 
-    Gson gson = new Gson();
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
+	Gson gson = new Gson();
 
-    //return the list of all the content
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
-        //code
-        response.getWriter().println("return all the content of the user (all users if he is admin)");
-    }
+	/**
+	 * For <b>registered user</b>, return the list of its own content ({@code Ptitu}).
+	 * For <b>administrator</b>, return the list of all contents.
+	 */
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		String mail = request.getParameter("mail");
+
+		Optional<User> optionalUser = (Storage.getAccounts()).stream()
+				.filter((a) -> (a.getMail()).equals(mail))
+				.findFirst();
+
+		if (optionalUser.isPresent()) {
+			response.getWriter().println(getAllContent(optionalUser.get()));
+		} else {
+			response.getWriter().println("No registered user (or administrator) with this mail...");
+		}
+	}
+
+	private String getAllContent(User user) {
+
+		List<Ptitu> content = new ArrayList<>();
+
+		switch(user.getRole()) {
+			case USER:
+				content = (Storage.getPtitu()).stream()
+						.filter(p -> (p.getOwnerMail()).equals(user.getMail()))
+						.collect(Collectors.toList());
+			case ADMIN:
+				content = (Storage.getPtitu());
+		}
+
+		return gson.toJson(content);
+	}
+
 }
-
--- src/main/java/PolyUrl/SeeDetails.java
+++ src/main/java/PolyUrl/SeeDetails.java
@@ -3,24 +3,72 @@ package PolyUrl;
 import com.google.gson.Gson;
 
 import javax.servlet.annotation.WebServlet;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
+
+import java.util.Optional;
+
 
 @WebServlet(name = "SeeDetails", value = "/seedetails")
 public class SeeDetails extends HttpServlet {
 
-    Gson gson = new Gson();
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
+	Gson gson = new Gson();
 
-    //return the detail of one purl
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
-        //code
-        //should check if the user is the one that created the purl (or admin)
-        response.getWriter().println("return the detail of one purl");
+	/**
+	 * For <b>registered user</b>, return the details of all accesses for one of his {@code Ptitu}.
+	 * For <b>administrator</b>, return the details of any content.
+	 */
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		String mail = request.getParameter("mail");
+		String pUrl = request.getParameter("purl");
+
+		Optional<User> optionalUser = (Storage.getAccounts()).stream()
+				.filter((a) -> (a.getMail()).equals(mail))
+				.findFirst();
+
+		if (optionalUser.isPresent()) {
+			response.getWriter().println(getDetails(optionalUser.get(), pUrl));
+		} else {
+			response.getWriter().println("No registered user (or administrator) with this mail...");
+		}
+	}
+
+	private String getDetails(User user, String pUrl) {
+
+		String details = new String();
+
+		Optional<Ptitu> optionalPtitu = (Storage.getPtitu()).stream()
+				.filter((p) -> (p.getUrl()).equals(pUrl))
+				.findFirst();
+
+		if (optionalPtitu.isPresent()) {
+			Ptitu ptitu = optionalPtitu.get();
+			switch(user.getRole()) {
+				case USER:
+					if ((ptitu.getOwnerMail()).equals(user.getMail())) {
+						details = gson.toJson(ptitu);
+					} else {
+						details = "The user registered with this mail did not create the purl in question.";
+					}
+				case ADMIN:
+					details = gson.toJson(ptitu);
+			}
+		} else {
+			details = "No ptit-u created with this URL...";
+		}
+
+		return details;
     }
-}
 
+}
