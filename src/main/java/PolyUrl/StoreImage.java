15271fc1d72d3bacff8209566cc48c39a6ac1424
Baptiste Frere <baptiste.frere@etu.unice.fr>
2019-11-16 15:57:14 +0100

    We store images !!

--- src/main/java/PolyUrl/StoreImage.java
+++ src/main/java/PolyUrl/StoreImage.java
@@ -1,25 +1,71 @@
 package PolyUrl;
 
+import com.google.appengine.repackaged.org.joda.time.DateTime;
+import com.google.appengine.repackaged.org.joda.time.DateTimeZone;
+import com.google.appengine.repackaged.org.joda.time.format.DateTimeFormat;
+import com.google.appengine.repackaged.org.joda.time.format.DateTimeFormatter;
+import com.google.cloud.storage.*;
+import com.google.cloud.storage.Storage;
 import com.google.gson.Gson;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+@MultipartConfig
 @WebServlet(name = "StoreImage", value = "/storeimage")
 public class StoreImage extends HttpServlet {
 
     Gson gson = new Gson();
 
     //add an image
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //code
+    /*@Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            CloudStorageHelper cloudStorageHelper = new CloudStorageHelper();
+            String x = cloudStorageHelper.getImageUrl(request,response,"poly_url_images");
+            //String x = cloudStorageHelper.uploadFile(request.getPart("file"),"poly_url_images");
+            response.getWriter().println(x);
+        }catch (Exception e){
+            response.getWriter().println("Erreur : "+e.getMessage());
+        }
+
+        //CloudStorageHelper cloudStorageHelper = new CloudStorageHelper();
+        //String resp = cloudStorageHelper.uploadFile(request.getPart(),"poly_url_images");
+
         // should store image then ask CreatePUrl class for the purl
-        response.getWriter().println("adds an image and returns the purl to access it");
+        //response.getWriter().println(resp);
+    }*/
+
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Storage storage = StorageOptions.getDefaultInstance().getService();
+        InputStream is = request.getPart("file").getInputStream();
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        byte[] readBuf = new byte[4096];
+        while (is.available() > 0) {
+            int bytesRead = is.read(readBuf);
+            os.write(readBuf, 0, bytesRead);
+        }
+
+        BlobInfo blobInfo = storage.create(BlobInfo
+                        .newBuilder("poly_url_images", request.getPart("file").getSubmittedFileName())
+                        // Modify access list to allow all users with link to read file
+                        //.setAcl(new ArrayList<>(Arrays.asList(Acl.of(Acl.User.ofAllUsers(), Acl.Role.READER))))
+                        .build(),
+                os.toByteArray());
+        response.getWriter().println(blobInfo.getMediaLink());
     }
 }
 
